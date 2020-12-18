46f5871a7a19bd8dae02ed1c8f923ad43c422635
Jorge Chirinos <chirinosjor@gmail.com>
2020-12-18 17:02:02 -0300

    DB changed to postgresql

--- db/migrate/20201207224203_create_orders.rb
+++ db/migrate/20201207224203_create_orders.rb
@@ -1,10 +1,10 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.string :order_status
-      t.string :order_code
-      t.string :order_name
-      t.string :order_app
+      t.text :order_status
+      t.text :order_code
+      t.text :order_name
+      t.text :order_app
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_07_224203) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "orders", force: :cascade do |t|
     t.string "order_status"
     t.string "order_code"
